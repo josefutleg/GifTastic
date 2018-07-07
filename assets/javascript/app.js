2a30d7ec21e2e43d78748780b7ef901a736e5d3d
josef utleg <josefutleg@gmail.com>
2018-07-07 09:57:54 -0700

    moved ajax to its own function. added cache:false to ajax. 1st attempt at solving mobile issue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 var topics = ['puppies', 'kitties', 'funny', 'scary', 'dinosaurs'];
 var button;
 var rating = [];
+var topicButton = [];
+var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=uREZoMju3bSu2Sho19kTyRgJr6Tvhfhg&limit=5&tag=" + topicButton + "&rating=" + rating;
+
 //giphy api key uREZoMju3bSu2Sho19kTyRgJr6Tvhfhg
 
 for (i in topics){
@@ -25,38 +28,44 @@ $('select').on('change', function(){
     rating = $(this).val();
 });
 
+function getGifs(){
+    queryURL;
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        cache: false
+    })
+    .then(function(response){
+    var imgDiv = $('<div>').addClass('image')
+    var makeImg = $('<img>').attr('src', response.data.fixed_height_small_still_url).attr('data-still',response.data.fixed_height_small_still_url).attr('data-animate',response.data.image_url).attr('data-state', 'still').addClass('gif');
+    var dataTag = $('<span>').text(response.data.title).attr('id','title');
+    var dataRating = $('<span>').text(rating).attr('id','rating');
+    var downloadTag = $(`<a href=${response.data.image_url} download>`).addClass('fas fa-arrow-alt-circle-down');
+    
+    if (rating == 'PG-13'){
+            dataRating.text(13);
+        }
+
+    imgDiv.append(dataRating);
+    imgDiv.append(downloadTag);
+    imgDiv.append(dataTag);
+    imgDiv.append(makeImg);
+    $('.gifContainer').prepend(imgDiv);
+    });
+}
+
 $('.topicsContainer').on('click', 'button', function(){
     // $('.gifContainer').empty();
 topicButton = $(this).text();
-var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=uREZoMju3bSu2Sho19kTyRgJr6Tvhfhg&limit=5&tag=" + topicButton + "&rating=" + rating;
+// var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=uREZoMju3bSu2Sho19kTyRgJr6Tvhfhg&limit=5&tag=" + topicButton + "&rating=" + rating;
 // console.log(queryURL);
     if (rating == ''){
         alert('choose a rating');
         return;
     }
 for (i=0; i<20; i++){
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function(response){
-        var imgDiv = $('<div>').addClass('image')
-        var makeImg = $('<img>').attr('src', response.data.fixed_height_small_still_url).attr('data-still',response.data.fixed_height_small_still_url).attr('data-animate',response.data.fixed_height_small_url).attr('data-state', 'still').addClass('gif');
-        var dataTag = $('<span>').text(response.data.title).attr('id','title');
-        var dataRating = $('<span>').text(rating).attr('id','rating');
-        var downloadTag = $(`<a href=${response.data.image_url} download>`).addClass('fas fa-arrow-alt-circle-down');
-        
-        if (rating == 'PG-13'){
-                dataRating.text(13);
-            }
-
-        imgDiv.append(dataRating);
-        imgDiv.append(downloadTag);
-        imgDiv.append(dataTag);
-        imgDiv.append(makeImg);
-        $('.gifContainer').prepend(imgDiv);
-        });
-    }
+    getGifs();
+}
 });
 
 $(document).on('click','img', function(){
